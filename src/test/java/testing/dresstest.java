183efe58fcf82201e2f11b0b1abd9bf0960e188d
vicky <vigneshvicky784533gmail.com>
2020-01-04 15:21:37 +0530

    cha

--- src/test/java/testing/dresstest.java
+++ src/test/java/testing/dresstest.java
@@ -22,7 +22,7 @@ public class dresstest {
 		Assert.assertTrue(bp.getTitlte().contains("Dresses"));	
 	}
 	@Test
-	public void verifyyyy123displayed() {
+	public void verifyyyy1236777displayed() {
 		Assert.assertTrue(dressob.getsize().isDisplayed(), "failed");	
 		Assert.assertTrue(dressob.getmedium().isDisplayed(), "failed");
 		Assert.assertTrue(dressob.getlarge().isDisplayed(), "failed");
